c8b90fa9db7d287df21da55ac50c02b211adee98
mohamadMehdiDabestani <mohamad1382mhd@gmail.com>
2022-05-23 00:57:48 -0700

    fixed support bug

--- components/common/Supports/timeFrame/index.jsx
+++ components/common/Supports/timeFrame/index.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import { Grid } from "@mui/material";
 import { MultipleSelect } from "@/components";
 import { useIndicator } from "@/hooks/useIndicator";
 import { useDispatch } from "react-redux";
 import { setSettings } from "@/redux/action/strategy/Actions";
+import ccxt from "ccxt";
+import { getCookie } from "cookies-next";
+
 export const TimeFrame = () => {
-  const { currentStrategy, timeFrames } = useIndicator();
+  const { currentStrategy } = useIndicator();
+  const ex = getCookie("exchange");
+  const [exchange, setExchange] = useState(new ccxt[ex]()); //eslint-disable-line
   const dispatch = useDispatch();
   const handleChange = (e) => {
     dispatch(
@@ -14,13 +20,14 @@ export const TimeFrame = () => {
       })
     );
   };
+  console.log("exchange" , exchange);
   return (
     <Grid item xl={4} lg={4} md={4} sm={6} xs={12}>
       <MultipleSelect
         value={
           currentStrategy.settings.find((e) => e.name === "timeFrames").value
         }
-        values={timeFrames}
+        values={Object.keys(exchange.timeframes)}
         change={handleChange}
         restSelect={{ name: "timeFrames" }}
       />
